123cc560a501bb77b15af6f911b643f3990b098a
Houcine <106538937+Hcn-88@users.noreply.github.com>
2023-01-02 18:06:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+
+  const [result, setResult] = useState("");
+  const operators = [".", "/", "*", "+", "-"];
+
+  const createNumbers = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button key={i} onClick={() => getValue(i.toString())}>
+          {i}
+        </button>
+      );
+    }
+    return digits;
+  };
+
+  const getValue = par => {
+
+    if(operators.includes(par) && result === "") return;
+    if(operators.includes(par) && operators.includes(result.slice(-1))) return;
+
+    const newResult = result + par
+    setResult(newResult);
+  }
+
+  const deleteValue = () => {
+
+    if(result === "") return;
+
+    const newValue = result.slice(0, -1);
+    setResult(newValue)
+  }
+
+  const doTheMath = () => {
+
+    if (result === "" || operators.includes(result.slice(-1))) return;
+
+    const calculateValue = Function(`return ${result}`)();
+    setResult(calculateValue);
+  }
+
+  const clearAll = () => {
+    const clear = "";
+    setResult(clear);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Calculator</h1>
+      <div className="display">
+        <input disabled type="text" value={result || "0"} />
+      </div>
+      <section>
+        <div className="clear">
+          <button onClick={clearAll}>Clear All</button>
+        </div>
+        <div>
+          <button onClick={deleteValue}>Del</button>
+          <button onClick={() => getValue(".")}>.</button>
+          <button onClick={() => getValue("/")}>/</button>
+          <button onClick={() => getValue("*")}>*</button>
+          <button onClick={() => getValue("+")}>+</button>
+          <button onClick={() => getValue("-")}>-</button>
+        </div>
+        <div>{createNumbers()}</div>
+        <div>
+          <button onClick={() => getValue("0")}>0</button>
+          <button onClick={() => getValue("00")}>00</button>
+          <button onClick={doTheMath}>=</button>
+        </div>
+      </section>
     </div>
   );
 }
